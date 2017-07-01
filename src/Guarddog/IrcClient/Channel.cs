0b665d77073ad54e0cf4ab66893bb24f353f0a6d
Antony Male <antony.male@gmail.com>
2017-07-01 10:27:40 +0100

    Listen to mutes

--- src/Guarddog/IrcClient/Channel.cs
+++ src/Guarddog/IrcClient/Channel.cs
@@ -53,16 +53,17 @@ namespace Guarddog.IrcClient
                         {
                             this.IsOp = mode.Set;
                         }
-                        else if (mode.Mode == 'b')
+                        else if (mode.Mode == 'b' || mode.Mode == 'q')
                         {
+                            var type = mode.Mode == 'b' ? UserBanType.Ban : UserBanType.Mute;
                             var newBanList = new List<UserBan>(this.BanList);
                             if (mode.Set)
                             {
-                                newBanList.Add(new UserBan(mode.Parameter, e.Who.Prefix, DateTime.UtcNow));
+                                newBanList.Add(new UserBan(mode.Parameter, e.Who.Prefix, DateTime.UtcNow, type));
                             }
                             else
                             {
-                                newBanList.RemoveAll(x => x.Mask == mode.Parameter);
+                                newBanList.RemoveAll(x => x.Mask == mode.Parameter && x.Type == type);
                             }
                             this.BanList = newBanList;
                         }
@@ -74,7 +75,7 @@ namespace Guarddog.IrcClient
                 var parameters = e.Message.Parameters;
                 if (parameters[1] == this.Name)
                 {
-                    var ban = new UserBan(parameters[2], parameters[3], DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(parameters[4])).DateTime);
+                    var ban = new UserBan(parameters[2], parameters[3], DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(parameters[4])).DateTime, UserBanType.Ban);
                     this.wipBanList.Add(ban);
                 }
 
@@ -84,16 +85,36 @@ namespace Guarddog.IrcClient
             {
                 if (e.Message.Parameters[1] == this.Name)
                 {
-                    this.BanList = this.wipBanList;
+                    this.BanList = this.wipBanList.Concat(this.BanList.Where(x => x.Type == UserBanType.Mute)).ToList();
                     this.wipBanList = new List<UserBan>();
                 }
                 return false;
             }, IrcCode.RPL_ENDOFBANLIST));
+            this.session.AddHandler(new IrcCodeHandler(e =>
+            {
+                var parameters = e.Message.Parameters;
+                if (parameters[1] == this.Name && parameters[2] == "q")
+                {
+                    var ban = new UserBan(parameters[3], parameters[4], DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(parameters[5])).DateTime, UserBanType.Mute);
+                    this.wipBanList.Add(ban);
+                }
+                return false;
+            }, IrcCode.RPL_MUTELIST));
+            this.session.AddHandler(new IrcCodeHandler(e =>
+            {
+                if (e.Message.Parameters[1] == this.Name && e.Message.Parameters[2] == "q")
+                {
+                    this.BanList = this.wipBanList.Concat(this.BanList.Where(x => x.Type == UserBanType.Ban)).ToList();
+                    this.wipBanList = new List<UserBan>();
+                }
+                return false;
+            }, IrcCode.RPL_ENDOFMUTELIST));
         }
 
         private void OnJoined()
         {
             this.session.Mode(this.Name, "+b");
+            this.session.Mode(this.Name, "+q");
         }
     }
 }
--- src/Guarddog/IrcClient/UserBan.cs
+++ src/Guarddog/IrcClient/UserBan.cs
@@ -4,17 +4,21 @@ using System.Text;
 
 namespace Guarddog.IrcClient
 {
+    public enum UserBanType { Ban, Mute }
+
     public class UserBan
     {
         public string Mask { get; }
         public string From { get; }
         public DateTime Date { get; }
+        public UserBanType Type { get; }
 
-        public UserBan(string mask, string from, DateTime date)
+        public UserBan(string mask, string from, DateTime date, UserBanType type)
         {
             this.Mask = mask ?? throw new ArgumentNullException(nameof(mask));
             this.From = from ?? throw new ArgumentNullException(nameof(from));
             this.Date = date;
+            this.Type = type;
         }
     }
 }
