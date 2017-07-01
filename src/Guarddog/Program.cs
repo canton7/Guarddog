c59a29fdeb271ae58cd69c60514f329b25ccebbf
Antony Male <antony.male@gmail.com>
2017-07-01 12:09:40 +0100

    Get Simple.Migrations in

--- src/Guarddog/Program.cs
+++ src/Guarddog/Program.cs
@@ -1,6 +1,12 @@
 ﻿using Guarddog.IrcClient;
 using IrcSays.Communication.Irc;
+using Microsoft.Data.Sqlite;
+using SimpleMigrations;
+using SimpleMigrations.Console;
+using SimpleMigrations.DatabaseProvider;
 using System;
+using System.Linq;
+using System.Reflection;
 
 namespace Guarddog
 {
@@ -8,6 +14,23 @@ namespace Guarddog
     {
         static void Main(string[] args)
         {
+            using (var connection = new SqliteConnection("Data Source=database.sqlite"))
+            {
+                var databaseProvider = new SqliteDatabaseProvider(connection);
+                var migrator = new SimpleMigrator(Assembly.GetEntryAssembly(), databaseProvider, new ConsoleLogger());
+                migrator.Load();
+                if (args.ElementAtOrDefault(0) == "migrate")
+                {
+                    var runner = new ConsoleRunner(migrator);
+                    runner.Run(args.Skip(1).ToArray());
+                    return;
+                }
+                else
+                {
+                    migrator.MigrateToLatest();
+                }
+            }
+
             var clientConfig = new ClientConfig()
             {
                 Server = "chat.freenode.net",
